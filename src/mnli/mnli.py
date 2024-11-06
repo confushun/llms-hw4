3b9e285e8c5a4f5b3c1b80867b90ad097da956ec
Anushka Mehta <aamehta@andrew.cmu.edu>
2024-11-06 10:30:01 -0500

    hacky fix

--- src/mnli/mnli.py
+++ src/mnli/mnli.py
@@ -45,6 +45,7 @@ def predict_labels(prompts: list[str]):
             results.append(int(label))
         except:
             print(f'{prompt} is not a valid label')
+            results.append(0)
 
     return results
 
